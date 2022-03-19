b015f76724bb410694fdf26101f6ec16f9ca5e9f
Jefferson <wilkermoair@gmail.com>
2022-03-19 10:43:29 -0300

    Nova rota

---
+++ resources/views/desc.blade.php
@@ -0,0 +1,31 @@
+<link href="css/desc.css" rel="stylesheet" type="text/css">
+
+@extends('layouts.app')
+
+@section('content')
+
+<div class="container">
+    <div class="row cont">
+        <div id="cards" class="col-sm-3">
+               <a href=""><img src="http://www.saofranciscodeassis.edu.br/wp-content/uploads/2020/12/FSFA-Default-300x150.png" alt=""></a>
+                <img src="http://www.saofranciscodeassis.edu.br/wp-content/uploads/2020/12/FSFA-Default-300x150.png" alt="">
+                <img src="http://www.saofranciscodeassis.edu.br/wp-content/uploads/2020/12/FSFA-Default-300x150.png" alt="">
+                <img src="http://www.saofranciscodeassis.edu.br/wp-content/uploads/2020/12/FSFA-Default-300x150.png" alt="">
+        </div>
+        <div class="col-sm-7    ">
+            <iframe 
+                    width="960" 
+                    height="615" 
+                    src="https://www.youtube.com/embed/NEFQXxU2DJU" 
+                    title="YouTube video player" 
+                    frameborder="0" 
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+                    allowfullscreen>
+            </iframe>
+        </div>
+    </div>
+</div>
+
+
+
+@endsection
